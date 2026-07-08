557b3ceb8751acba7ecddcef0173c98ead73fc6d
mukhiddinova1712-star <mukhiddinova1712@gmail.com>
2026-07-08 07:10:54 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
   images: {
     domains: ['images.unsplash.com', 'source.unsplash.com', 'picsum.photos', 'www.themealdb.com'],
     unoptimized: true,
